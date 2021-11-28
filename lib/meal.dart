4ff69bcd56d40d0108b0cb9cf428daa59681350e
Denise Oliveira <dedacpo@gmail.com>
2021-11-28 18:56:28 -0300

    Step 7: build the meal withe param passed

--- lib/meal.dart
+++ lib/meal.dart
@@ -30,6 +30,111 @@ class Meal {
 
 }
 
-Widget widgetMeal(){
-  return Text('WidgetMeal');
+Widget widgetMeal(Meal meal, BuildContext context){
+  return Column(children: [
+     Image.network(meal.imgUrl),
+      Text(
+        meal.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Theme.of(context).primaryColorDark,
+            fontSize: 20,
+            fontWeight: FontWeight.bold),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          meal.description,
+          textAlign: TextAlign.center,
+          style: TextStyle(height: 2),
+        ),        
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '${meal.kcal.toString()}',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold),
+                  ),
+                   Text(
+                    'kcal',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ]),
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '${meal.prot.toString()}g',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'prot',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ]),
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '${meal.carbs.toString()}g',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'carbs',
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ]),
+          ),
+        ],
+      )
+  ],);
 }
