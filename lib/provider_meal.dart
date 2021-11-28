a98290ffdea763746e2e3597324bd9760a84c62a
Denise Oliveira <dedacpo@gmail.com>
2021-11-28 20:01:46 -0300

    Step 23: Create the provider to post the received meal to mockAPI

--- lib/provider_meal.dart
+++ lib/provider_meal.dart
@@ -12,4 +12,17 @@ class Meals{
     final response = await http.get(Uri.parse(_apiUrl));
     return(json.decode(response.body) as List).map((e) => Meal.fromJson(e)).toList();
   }
+
+  Future<Meal> postMeal(Map<String, dynamic> meal) async {
+    final response = await http.post(
+      Uri.parse(_apiUrl),
+      body: json.encode(meal),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    return Meal.fromJson(json.decode(response.body));
+  }
 }
